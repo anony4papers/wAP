392ae817a3a5836dcfb0611b0fa63637b1b14abf
anony4papers <72099892+anony4papers@users.noreply.github.com>
2020-09-29 15:24:57 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,10 +7,10 @@ ATTACK_MTD='salt_numberchange' #'cw_targetclsmiss', 'noise_numberchange', 'brigh
 #'cw_targetclsmiss', 'noise_numberchange', 'bright_numberchange'
 SQUZZEZ_MTD='bit_7'
 
-# python generate_adv.py --imgs-dir ${IMGS_DIR} --attack-mtd ${ATTACK_MTD}
+python generate_adv.py --imgs-dir ${IMGS_DIR} --attack-mtd ${ATTACK_MTD}
 
-# python generate_squeeze.py ori --imgs-dir ${IMGS_DIR} --squeeze-type ${SQUZZEZ_MTD}
-# python generate_squeeze.py adv --imgs-dir ${IMGS_DIR} --squeeze-type ${SQUZZEZ_MTD}
-# python generate_detection_results.py --imgs-dir ${IMGS_DIR} --squeeze-type ${SQUZZEZ_MTD}
-# python new_cal_wap.py --imgs-dir ${IMGS_DIR} --squeeze-type ${SQUZZEZ_MTD}
+python generate_squeeze.py ori --imgs-dir ${IMGS_DIR} --squeeze-type ${SQUZZEZ_MTD}
+python generate_squeeze.py adv --imgs-dir ${IMGS_DIR} --squeeze-type ${SQUZZEZ_MTD}
+python generate_detection_results.py --imgs-dir ${IMGS_DIR} --squeeze-type ${SQUZZEZ_MTD}
+python new_cal_wap.py --imgs-dir ${IMGS_DIR} --squeeze-type ${SQUZZEZ_MTD}
 python new_eval.py --imgs-dir ${IMGS_DIR} --squeeze-type ${SQUZZEZ_MTD}
